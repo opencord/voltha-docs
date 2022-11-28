1965565c1e3bd9e2ad2ebbe1666b423d5ef321ea
Joey Armstrong <joey@opennetworking.org>
2022-11-28 09:00:28 -0500

    VOL-4840 - Makefile edits
    VOL-4874, VOL-4877, VOL-4840
    
    howto/edit_voltha_docs.rst
    --------------------------
       * Also need to run "make docs" to generate html content.
    
    Makefile
    --------
       * (local use) fixed non-functional makefile targets.
       * Begin splitting monolithic makefile into reusable libs.
       * Extended help to document all targets (make help VERBOSE=1)
       * Extended lint to support more targets.
       * Replace wildcard target (%) with a list of sphinx-build named targets:
         + Amusing behavior but "make clean" should not initiate "gendocs".
         + Likewise "make lint" should not attempt to generate documentation.
       * Introduced simple patching logic for virtualenv.  Local users are
         now able to use make clean, lint, etc with a newer interpreter.
         Baby steps toward VOL-4874.
       * Conditional make vars added:
         + NO_PATCH=1
         + NO_OTHER_REPO_DOCS=1 (venv patching not yet supported by foreign repos).
    
    makefiles/consts.mk
    -------------------
       * basic values: space, dot, null, HIDE.
    
    makefiles/help/include.mk
    -------------------------
       * makefile target help with context.
    
    makefiles/lint/*.mk
    -------------------
       * lib makefiles capturing syntax checking logic.
    
    makefiles/patches/include.mk
    ----------------------------
       * Targets for creation and applying python virtualenv patches.
    
    patches/*/patch
    patches/python_310_migration.sh
    -------------------------------
       * Logic used to support dynamic python module pathching in a virtual env directory.
       * Module edits are trivial -- attempt to import new modules and fallback to existing import syntax.
    
    Change-Id: I437289a751423217b4193b75b62c4a295db18ffd

---
+++ makefiles/voltha/docs-catchall-targets.mk
@@ -0,0 +1,44 @@
+# -*- makefile -*-
+# -----------------------------------------------------------------------
+# Copyright 2022 Open Networking Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -----------------------------------------------------------------------
+
+voltha-docs-catchall += docs
+voltha-docs-catchall += html
+voltha-docs-catchall += dirhtml
+voltha-docs-catchall += singlehtml
+voltha-docs-catchall += pickle
+voltha-docs-catchall += json
+voltha-docs-catchall += htmlhelp
+voltha-docs-catchall += qthelp
+voltha-docs-catchall += devhelp
+voltha-docs-catchall += epub
+voltha-docs-catchall += latex
+voltha-docs-catchall += latexpdf
+voltha-docs-catchall += latexpdfja
+voltha-docs-catchall += text
+voltha-docs-catchall += man
+voltha-docs-catchall += texinfo
+voltha-docs-catchall += info
+voltha-docs-catchall += gettext
+voltha-docs-catchall += changes
+voltha-docs-catchall += xml
+voltha-docs-catchall += pseudoxml
+voltha-docs-catchall += linkcheck
+voltha-docs-catchall += doctest
+voltha-docs-catchall += coverage
+# voltha-docs-catchall += clean
+
+# [EOF]
